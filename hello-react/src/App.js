3d093a0fdb289853d0185e5ec1ff6642b3a325ed
ine <enchanted13th@gmail.com>
2021-03-10 13:22:31 +0900

    add Chapter06

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,18 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react';
-import ScrollBox from './05/ScrollBox';
+import IterationSample from './06/IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
