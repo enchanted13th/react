0c1df59c3b3a6a057ea32d59431c65cbf74ad5f4
ine <enchanted13th@gmail.com>
2021-03-10 15:41:41 +0900

    add Chapter07

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,11 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react';
-import IterationSample from './06/IterationSample';
+import LifeCycleSample from './07/LifeCycleSample';
+import ErrorBoundary from './07/ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
