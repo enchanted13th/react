147c099787ebf3a417d4d8fd7ee2a0c9acd2a763
ine <enchanted13th@gmail.com>
2021-03-10 10:52:49 +0900

    add Chapter05

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from 'react';
-import EventPractice from './04/EventPractice';
+import React, { Component } from 'react';
+import ScrollBox from './05/ScrollBox';
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
